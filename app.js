4bec5c1bdcae55e74eb45482e290749b9f50381d
MariLezama <lezamamariangel082001@gmail.com>
2023-06-09 21:02:15 -0500

    agregue archivos nuevos

---
+++ app.js
@@ -0,0 +1,15 @@
+//alert("Hola desde MakeItReal");
+
+//Esto es un comentario para saber que estoy haciendo
+
+console.log ("Hola Mundo"); 
+console.log ('Este es la frase de la \'linea\' 6');
+console.log ("Esta es 'la frase' de la \"linea\" 7");
+
+//Concatenando String
+console.log ("Hola " + "desde " + "mi " + "pc")
+
+/*Este es un comentario en parrafo que no quiero que se vea*/
+
+
+
